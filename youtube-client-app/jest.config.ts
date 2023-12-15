0b40a43832740acb9b02e04b028d3f8d62793dd6
Quaqva <sanek15056@gmail.com>
2023-12-15 18:55:49 +0400

    feat: delete karma+jasmine and configure jest

---
+++ youtube-client-app/jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  verbose: true,
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.html$',
+    },
+  },
+};
+
+export default config;
